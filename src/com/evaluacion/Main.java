8b626b7cae2356a07be6a5f2b28ed93b1c2be1a4
Gary Yaral <garyelemusic@gmail.com>
2021-09-11 13:34:05 -0500

    finish

--- src/com/evaluacion/Main.java
+++ src/com/evaluacion/Main.java
@@ -30,39 +30,60 @@ public class Main {
         EstBombero estacion2 = new EstBombero();
         estacion2.setCodigoEstacion(2);
         double[] coordenadas2 = {-2,-2.7};
-        estacion1.setCoordenadasEstacion(coordenadas2);
+        estacion2.setCoordenadasEstacion(coordenadas2);
         
         
         //estacion 3
         EstBombero estacion3 = new EstBombero(); 
         estacion3.setCodigoEstacion(2);
         double[] coordenadas3 = {-3,-2};
-        estacion2.setCoordenadasEstacion(coordenadas3);
+        estacion3.setCoordenadasEstacion(coordenadas3);
 
         //guardamos las estaciones en una coleccion llamada EST
-        EstBombero EST[] = new EstBombero[3];
-        EST[0] = estacion1;
-        EST[1] = estacion2;
-        EST[2] = estacion3;
+        EstBombero EST[] = {estacion1,estacion2,estacion3};
         
         //creamos el siniestro y le pasamos sus coordenadas del incendio
         Siniestro incendio = new Siniestro();
         double[]coordernadasSiniestro = {-1.5,-1.5};
         incendio.setCoordenadasIncendio(coordernadasSiniestro);
         
-        double[] estacionAtendera = new double[2];
-        
         double coordenadaSiniestroX = (incendio.getCoordenadasIncendio()[0]);
         double coordenadaSiniestroY = (incendio.getCoordenadasIncendio()[1]);
-        double[] coordenadaEstacion = new double[2];
+        double coordenadaEstacionX;
+        double coordenadaEstacionY;
         
-        double distancia = 0;
-        
-        for(EstBombero estacion:EST){
-            
-       
+        double[] distancia = new double[3];
+
+
+        for(int i = 0; i < EST.length ; i++ ){
+            coordenadaEstacionX = EST[i].getCoordenadasEstacion()[0];
+            coordenadaEstacionY = EST[i].getCoordenadasEstacion()[1];
+            double distanciaX = coordenadaSiniestroX - coordenadaEstacionX;
+            double distanciaY = coordenadaSiniestroY - coordenadaEstacionY;      
+            distancia[i] = Math.sqrt(Math.pow(distanciaX,2) + Math.pow(distanciaY,2));     
             
         }
+        
+        double menor = 0;
+        int indice = 0;
+        for(int i = 0; i < distancia.length; i++){
+            
+            if(i == 0){
+                menor = distancia[i];      
+            }else{
+            
+                if(distancia[i] < menor ){
+                    menor = distancia[i];
+                    indice = i;
+                }
+                    
+            }
+        }
+        int estacionAtendera = indice + 1;
+        System.out.println("Atenderá el incendio");
+        System.out.println("Estación: " + estacionAtendera);
+        System.out.println("CoordenadaX: "+ EST[indice].getCoordenadasEstacion()[0]);
+        System.out.println("CoordenadaY: "+ EST[indice].getCoordenadasEstacion()[1]);
         
         
     }
